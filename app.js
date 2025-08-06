1ad05414399172f1300df60cda6443cfb99e21af
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-08-06 22:00:59 +0530

    update app.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: [
+      "https://filestorage.zyva-designs.com",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3002",
